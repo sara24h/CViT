e8c64c5db3e75c65fc5a3b6884824067a4fedb20
Sara <51385797+sara24h@users.noreply.github.com>
2024-08-23 21:18:31 +0330

    Update cvit_train.py

--- cvit_train.py
+++ cvit_train.py
@@ -11,7 +11,7 @@ from time import perf_counter
 from datetime import datetime
 from model.cvit import CViT
 from helpers.loader import load_data
-
+import optparse
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
